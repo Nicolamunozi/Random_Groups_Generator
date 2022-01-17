f34925d0e1cf73428d61673910b30684705238f0
Nicolás Muñoz <80085614+Nicolamunozi@users.noreply.github.com>
2022-01-17 00:02:24 -0300

    Update main.r
    Tidy up comments

--- groupsrandomizer/R/main.r
+++ groupsrandomizer/R/main.r
@@ -4,8 +4,10 @@ library(writexl)
 library(tcltk)
 #TODO: Crear ambiente del paquete.
 
-#functions:
-#Utils
+#Functions:
+#I) Utils
+
+#a) Length of the groups.
 
 len_groups <- function(data){
 
@@ -14,18 +16,14 @@ len_groups <- function(data){
   }
 }
 
-#Utils
-#TODO: Funcion para identificar si es Linux o Windows
-
-#If isWindows == TRUE -> Haz esta wea
-#Else if -> haz la wea nueva
+#b) System Identify
 isWindows <- function(){
   unname(Sys.info()["sysname"]) == "Windows"
 }
 
 #Load Data:
 
-#TODO: Generalizar tipo de archivo, agregar csv y otros formatos excel.
+#TODO: Generalizar tipo de archivo (agregar csv y otros formatos excel)
 
 
 load_data <- function(file=file.choose()){
@@ -36,8 +34,8 @@ load_data <- function(file=file.choose()){
 
 #Preprocess Data:
 #Notar que la data viene en un orden, que es "Nombre","Apellido","Mail"
-#
 #TODO: Generalizar orden y cantidad de columnas
+
 process_data <- function(data){
           n_filas <- dim(data)[1]
           ID <- as.double(c(1:n_filas))
@@ -54,6 +52,7 @@ process_data <- function(data){
 #Generating groups:
 
 #TODO: Vectorizar
+
 get_groups <- function(data, group_zise){
                 groups <- list()
                 len_groups <- dim(data)[1]/group_zise
@@ -128,9 +127,6 @@ data_formating <- function(data, variables = c(5,2,3,4), as_df = FALSE){
 }
 
 #Data exporting:
-#(should it be for more formats?)
-#TODO: Generalizar tipo output (FORMATO).
-#TODO: Generalizar Path
 #TODO: Generalizar tipo y nombre del archivo output.
 
 data_exporting <- function(data, name="groups.xlsx"){
@@ -144,6 +140,7 @@ data_exporting <- function(data, name="groups.xlsx"){
 }
 
 #Integrated function:
+#TODO: Integrar fill_groups.
 
 randomizer <- function(group_zise){
 
