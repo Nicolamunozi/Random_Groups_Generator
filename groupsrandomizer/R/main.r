59760d3e000bb694ea83b4615311a41cd2aa334a
Nicolás Muñoz <80085614+Nicolamunozi@users.noreply.github.com>
2022-01-16 21:32:22 -0300

    Update main.r

--- groupsrandomizer/R/main.r
+++ groupsrandomizer/R/main.r
@@ -121,8 +121,13 @@ data_formating <- function(data, variables = c(5,2,3,4), as_df = FALSE){
 #Data exporting:
 #(should it be for more formats?)
 #TODO: Generalizar tipo output (FORMATO).
-
-
+data_exporting <- function(data, name="groups.xlsx"){
+  if (isWindows() == FALSE){
+    path <- tk_choose.dir()
+  }
+  else{
+    path <- choose.dir()
+  }
   write_xlsx(data, path = file.path(path, name))
 }
 
